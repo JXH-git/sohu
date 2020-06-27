9e868a018b7b855192c0859e16fe84a92cb7d21f
JXH-git <2427565275@qq.com>
2020-06-27 17:21:30 +0800

    二级页面的爬取

--- tt/spiders/toutiao.py
+++ tt/spiders/toutiao.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import urllib.request
+
 import scrapy
+from scrapy import Request
 
 from tt.items import TtItem
-
+from lxml import etree
 
 class ToutiaoSpider(scrapy.Spider):
     name = "sohu"
@@ -10,6 +13,16 @@ class ToutiaoSpider(scrapy.Spider):
     start_urls = (
         'http://www.sohu.com/',
     )
+
+    def sec(self, url):
+        sec_html = urllib.request.urlopen(url)
+        sec_response = sec_html.read().decode('utf-8')
+        etree_sec_response = etree.HTML(sec_response)
+        # content = etree_sec_response.xpath('article[@class="article"]/p/text()')
+        # print(content)
+        print(sec_response[
+              sec_response.find('<article') + 1 : sec_response.rfind('</article>')])
+        print('-----------------------------------')
 
     def parse(self, response):
         news = response.xpath('//div[@class="news"]/p/a')
@@ -19,3 +32,5 @@ class ToutiaoSpider(scrapy.Spider):
             item['href'] = new.xpath('./@href').extract()[0]
             print('+++++++++++++++++++++++++++++++++++++++++')
             yield item
+            if item['href']:
+                yield Request(item['href'], callback=self.sec(item['href']))
